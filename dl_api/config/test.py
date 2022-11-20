11129a192f701e07b7652709521b23c207b0d278
Alexis Andrade <alexis.martinez.contacto@gmail.com>
2022-11-20 13:56:37 -0600

    Create server environments: deploy, dev, test

---
+++ dl_api/config/test.py
@@ -0,0 +1,16 @@
+"""
+Default settings for test environment.
+"""
+from .deployment import *
+
+# Secret key for dev env
+SECRET_KEY = '}uqmE!Vu/S^W@6zBBGRTom!Fdj[OqTmB^X$[Ivq{3mRNVm@jg@;ofS0ee;Elr#nzGy'
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
+}
