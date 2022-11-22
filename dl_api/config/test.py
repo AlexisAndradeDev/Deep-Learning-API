3ca673b1691cb1b14c83152f841549538ab42a39
Alexis Andrade <alexis.martinez.contacto@gmail.com>
2022-11-21 22:00:52 -0600

    Setup Django workflow and unit tests

--- dl_api/config/test.py
+++ dl_api/config/test.py
@@ -11,6 +11,6 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'test_db.sqlite3',
+        'NAME': BASE_DIR / '/tests/test_db.sqlite3',
     }
 }
